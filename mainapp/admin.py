ba12d135df51aaa5a685539ab9a6d8261d6a62c7
Тимофей Важенин <vazhenin.t@gmail.com>
2024-05-10 15:02:45 +0300

    HW_5

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,25 +2,42 @@ from django.contrib import admin
 from .models import User, Product, Order
 
 
+@admin.action(description='Сбросить количество товаров')
+def reset_quantity(modeladmin, request, queryser):
+    queryser.update(quantity = 0)
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'phone', 'email')
+    list_filter = ('name', 'phone')
     search_fields = ('id', 'name', 'phone')
     readonly_fields = ('id', 'date_registration')
-
+    search_fields = ('name',)
+    search_help_text = 'Поиск по логину'
+    ordering = ('id', )
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'quantity')
+    list_filter = ('name','price', 'quantity')
     search_fields = ('id', 'name', 'quantity')
     readonly_fields = ('id', 'date_add')
+    search_fields = ('name',)
+    search_help_text = 'Поиск по названию товара'
+    ordering = ('id', )
+    actions = [reset_quantity]
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'client', 'total_price')
+    list_filter = ('client',)
     search_fields = ('id', 'client', 'products')
     readonly_fields = ('id', 'date_ordered')
+    search_fields = ('client',)
+    search_help_text = 'Поиск по клиенту'
+    ordering = ('-date_ordered', )
 
     fieldsets = [
         (None, { "fields": ['id', 'client', 'date_ordered'],},),
--- mainapp/models.py
+++ mainapp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=200, db_index=True)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    address = models.CharField(max_length=200)
+    email = models.EmailField(blank = True)
+    phone = models.CharField(max_length=15, blank = True)
+    address = models.CharField(max_length=200, blank = True)
     date_registration = models.DateField(auto_now_add=True)
 
     class Meta:
@@ -18,9 +18,9 @@ class User(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200, db_index=True, verbose_name = 'Название')
-    desc = models.TextField(verbose_name = 'Описание')
-    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name = 'Цена')
-    quantity = models.DecimalField(max_digits=8, decimal_places=2, verbose_name = 'Количество')
+    desc = models.TextField(default = 'Описание пока не добавлено', verbose_name = 'Описание', blank = True)
+    price = models.DecimalField(default = 1000.00, max_digits=8, decimal_places=2, verbose_name = 'Цена')
+    quantity = models.DecimalField(default = 1.00, max_digits=8, decimal_places=2, verbose_name = 'Количество')
     date_add = models.DateField(auto_now_add=True, verbose_name = 'Дата создания')
     image = models.ImageField(verbose_name = 'Изображение товара',upload_to='products/', blank=True)
 
@@ -36,7 +36,7 @@ class Product(models.Model):
 class Order(models.Model):
     client = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    total_price = models.DecimalField(default = 1000.00, max_digits=8, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
 
     class Meta:
